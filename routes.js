d4127c0176e77ab1d1285845254dcd9dc93b9f6b
Arnoldo Cortez y Quevedo Cansino <arnoldo.cortez@theksquaregroup.com>
2021-04-03 19:30:08 -0600

    creating a notes api

---
+++ routes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err) return res.send(err);
+
+    conn.query('SELECT * FROM notes', (err, rows) => {
+      if(err) return res.send(err);
+
+      res.json(rows);
+    })
+  });
+});
+
+routes.get('/:id', (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err) return res.send(err);
+
+    conn.query('SELECT * FROM notes WHERE id = ?', [req.params.id], (err, rows) => {
+      if(err) return res.send(err);
+
+      res.json(rows);
+    })
+  });
+});
+
+routes.post('/', (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err) return res.send(err);
+
+    conn.query('INSERT INTO notes set ?', [req.body], (err, rows) => {
+      if(err) return res.send(err);
+
+      res.send("note added!!");
+    })
+  });
+});
+
+routes.delete('/:id', (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err) return res.send(err);
+
+    conn.query('DELETE FROM notes WHERE id = ?', [req.params.id], (err, rows) => {
+      if(err) return res.send(err);
+
+      res.send("notes excluded!!");
+    })
+  });
+});
+
+routes.put('/:id', (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err) return res.send(err);
+
+    conn.query('UPDATE notes set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+      if(err) return res.send(err);
+
+      res.send("notes updated!!");
+    })
+  });
+});
+
+
+module.exports = routes;
