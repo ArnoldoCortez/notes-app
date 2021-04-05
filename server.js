eb08f667006a3f3882d0a715be8daa143c3bb100
Arnoldo Cortez y Quevedo Cansino <arnoldo.cortez@theksquaregroup.com>
2021-04-05 14:13:58 -0500

    changing port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const myconn = require('express-myconnection')
 const routes = require('./routes');
 
 const app = express();
-app.set('port', process.env.PORT || 9000);
+app.set('port', process.env.PORT || 8080);
 
 const dbOptions = {
   host: 'notesdb.cqtdygblynpw.us-east-2.rds.amazonaws.com',
